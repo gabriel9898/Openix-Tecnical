7a7b60e9fdf336a382caa5e11b39198a48c8afb3
Omar Cruz <omar@openix.com.ar>
2022-08-08 09:19:52 -0300

    feat(exercise 1.1): dom management from js

---
+++ 1.1/script.js
@@ -0,0 +1,10 @@
+const films = [
+  "./assets/images/angry-bird.jpg",
+  "./assets/images/blacklight.jpg",
+  "./assets/images/harry-potter.jpg",
+  "./assets/images/marvels-avengers.jpg",
+  "./assets/images/matrix-resurrections.jpg",
+  "./assets/images/resident-evil.jpg",
+  "./assets/images/venom-matanza.jpg",
+  "./assets/images/x-men-days-of-future-past.jpg",
+];
---
+++ readme.md
@@ -0,0 +1,9 @@
+Para realizar la siguiente prueba técnica se recomienda tener instalado node en la maquina.
+
+Para poder levantar los ejercicios rápidamente el ejercicio.
+
+1.1
+
+Se recomienda usar el siguiente comando
+
+`npx live-server $path-folder`
